06db183b754e1ca11ffc32b2e4f087bf740d97f4
FerZablah <zablah.developer@gmail.com>
2020-02-10 15:19:26 -0600

    created multiplicar method

--- Hola.java
+++ Hola.java
@@ -3,4 +3,9 @@ import java.io.*;
 class Hola{
    public static void main (String arg[]){
       System.out.println("Ingrese opcion");
+   }
+   public static double multiplicar(double a, double b){
+      return a*b;
+   }
+
 }
