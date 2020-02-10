56dec386c3dc28354330a12fe8f6a05a67c3c498
FerZablah <zablah.developer@gmail.com>
2020-02-10 14:58:01 -0600

    added adios mundo cruel 2

--- Hola.java
+++ Hola.java
@@ -2,6 +2,6 @@ import java.io.*;
 
 class Hola{
    public static void main (String arg[]){
-      System.out.println("Adios mundo cruel");
+      System.out.println("Adios mundo cruel 2");
    }
 }
