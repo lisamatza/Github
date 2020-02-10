8c1f6fed983ac6e2cc4ea53c72f778e5d6a642cc
LisaMatzapan <lisa@MacBook-Pro-de-Lisa.local>
2020-02-10 15:02:28 -0600

    puse mi nombre

--- Hola.java
+++ Hola.java
@@ -3,6 +3,6 @@ import java.io.*;
 class Hola{
    public static void main (String arg[]){
       System.out.println("Adios mundo");
-      System.out.println("Hola mundo");
+      System.out.println("LisaMatza");
    }
 }
